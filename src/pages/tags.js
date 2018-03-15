ae9a1de33ff841e3fe080533f7f5f9f8b878c258
sigoden <sigoden@gmail.com>
2018-03-15 20:03:20 +0800

    add tags

---
+++ src/pages/tags.js
@@ -0,0 +1,78 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import Link, { navigateTo } from "gatsby-link"
+import { TagCloud } from "react-tagcloud"
+import kebabCase from "lodash/kebabCase"
+
+import Container from "../components/container"
+
+const Tag = (tag, size, color) => (
+  <span
+    key={tag.value}
+    style={{
+      fontSize: `${size}px`,
+      color,
+      cursor: 'pointer',
+      margin: '0px 3px',
+      verticalAlign: 'middle',
+      display: 'inline-block'
+    }}
+  >
+    {tag.value}
+  </span>
+)
+
+const TagsPage = ({
+  data: { allMarkdownRemark: { group }, site: { siteMetadata: { title } } },
+}) => (
+  <div>
+    <Helmet title={title} />
+    <Container className="tag" css={{ paddingBottom: `0 !important` }}>
+      <TagCloud tags={group.map(tag => ({value: tag.fieldValue, count: tag.totalCount}))}
+        minSize={30}
+        maxSize={80}
+        renderer={Tag}
+        onClick={tag => navigateTo(`/tags/${kebabCase(tag.value)}/`)}
+      />
+    </Container>
+  </div>
+)
+
+TagsPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      group: PropTypes.arrayOf(
+        PropTypes.shape({
+          fieldValue: PropTypes.string.isRequired,
+          totalCount: PropTypes.number.isRequired,
+        }).isRequired
+      ),
+    }),
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }),
+    }),
+  }),
+}
+
+export default TagsPage
+
+export const pageQuery = graphql`
+  query TagsQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      limit: 2000
+    ) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
