8308cd6791d020d8a7deb89953d382e122a95bf1
sigoden <sigoden@gmail.com>
2019-04-04 00:14:58 +0800

    add github action to deploy

---
+++ .github/action-deploy/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+STAGE_DIR=${GITHUB_WORKSPACE}/stage
+GHPAGES_BRANCH=gh-pages
+TARGET_REPO_URL="git@github.com:${GITHUB_REPOSITORY}.git"
+
+if [ -z "$RENDER_BRANCH" ]; then
+    echo "Set the RENDER_BRANCH env variable."
+    exit 1
+fi
+if [ -n "${GIT_DEPLOY_KEY}" ]; then
+    mkdir /root/.ssh
+    ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts && \
+    echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
+    chmod 400 /root/.ssh/id_rsa
+fi
+
+git submodule init
+git submodule update --remote --recursive
+echo "cloned the repo"
+
+npm install --silent
+npx gatsby build --prefix-paths
+echo "built with gatsby"
+
+mv public /tmp
+
+cd /tmp/public
+
+git init
+git remote add deploy $TARGET_REPO_URL
+git checkout --orphan $GHPAGES_BRANCH 
+git add .
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git commit -q -m "Automated deployment to GitHub Pages"
+git push deploy $GHPAGES_BRANCH --force
+echo "deployed to gh pages"
---
+++ .github/main.workflow
@@ -0,0 +1,41 @@
+workflow "Build & Deploy: masater" {
+  resolves = [
+    "Deply: push master"
+  ]
+  on = "push"
+}
+
+workflow "Build & Deploy: render" {
+  resolves = [
+    "Deply: push render"
+  ]
+  on = "push"
+}
+
+action "Deply: push master" {
+  uses = "./.github/action-deploy"
+  env = {
+    RENDER_BRANCH = "render"
+  }
+  secrets = ["GIT_DEPLOY_KEY"]
+  needs = ["Filters: push master"]
+}
+
+action "Deply: push render" {
+  uses = "./.github/action-deploy"
+  env = {
+    RENDER_BRANCH = "render"
+  }
+  secrets = ["GIT_DEPLOY_KEY"]
+  needs = ["Filters: push render"]
+}
+
+action "Filters: push render" {
+  uses = "actions/bin/filter@3c98a2679187369a2116d4f311568596d3725740"
+  args = "branch render"
+}
+
+action "Filters: push master" {
+  uses = "actions/bin/filter@3c98a2679187369a2116d4f311568596d3725740"
+  args = "branch master"
+}
